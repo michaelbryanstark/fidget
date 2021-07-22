5e56e2048b588452cfcdbfa5b9c355f3304f9cd8
Michael Stark <michael.starkb@gmail.com>
2021-07-22 11:49:17 -0400

    create function for oauth in passport.js

--- auth/passport.js
+++ auth/passport.js
@@ -11,7 +11,23 @@ passport.use(
         callbackURL: process.env.GOOGLE_CALLBACK,
       },
       function (accessToken, refreshToken, profile, cb) {
-        // a user has logged in with OAuth...
+        User.findOne({ googleId: profile.id }, (err, user) => {
+            console.log(err);
+            if (err) return cb(err);
+            if (user) {
+                return cb(null, user);
+            } else {
+                const newUser = new User({
+                    name: profile.displayName,
+                    email: profile.emails[0].value,
+                    googleId: profile.id,
+                });
+                newUser.save(function (err) {
+                    if (err) return cb(err);
+                    return cb(null, newUser);
+                });
+            }
+        });
       }
     )
   );
