09af7968b0b0d045b7afd35b7f1690fabadd530a
Michael Stark <michael.starkb@gmail.com>
2021-07-22 16:08:48 -0400

    add login/out to index route

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,22 @@ router.get('/', function(req, res) {
     });
 });
 
+router.get(
+    '/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+  );
+
+router.get(
+    '/oauth2callback',
+    passport.authenticate('google', {
+        successRedirect: '/user',
+        failureRedirect: '/',
+    })
+);
+
+router.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
 module.exports = router;
