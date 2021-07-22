3bd5d86132fd28281a8d0a4d012a0b03c816b0cb
Michael Stark <michael.starkb@gmail.com>
2021-07-22 11:19:44 -0400

    create const user

--- auth/passport.js
+++ auth/passport.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
 
 const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const User = require('../models');
 
 passport.use(
     new GoogleStrategy(
