37e7d4abe4e19c9d737bed5f66d52636bb2a1504
Michael Stark <michael.starkb@gmail.com>
2021-07-22 11:14:26 -0400

    create passport.use

--- auth/passport.js
+++ auth/passport.js
@@ -1,3 +1,16 @@
 const passport = require("passport");
 
 const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+
+passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_SECRET,
+        callbackURL: process.env.GOOGLE_CALLBACK,
+      },
+      function (accessToken, refreshToken, profile, cb) {
+        // a user has logged in with OAuth...
+      }
+    )
+  );
