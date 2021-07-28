ae7b537067e54fb7e1c58f1155cfb0285a28d5d8
Michael Stark <michael.starkb@gmail.com>
2021-07-28 14:20:06 -0400

    trying to get user page to display user model data

---
+++ routes/user.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const passport = require('passport');
+const { db } = require('../models/index');
+
+
+router.get('/', function(req, res) {
+    res.render('index', {
+        user:req.user,
+        posts: req.posts,
+    });
+});
+
+router.get('/user', (req, res) => {
+    res.render('user', {
+        user:req.user,
+        posts:req.posts,
+    });
+});
+
+
+router.get(
+    '/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+  );
+
+  router.get(
+    '/oauth2callback',
+    passport.authenticate('google', {
+
+        successRedirect: '/', 
+        failureRedirect: '/',
+    })
+)
+
+router.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+module.exports = router;
