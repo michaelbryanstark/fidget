4b63bf1b3cd74112d51c4b2e66004730532244e5
Michael Stark <michael.starkb@gmail.com>
2021-07-28 00:19:32 -0400

    modified post controller to render main page

--- controllers/posts.js
+++ controllers/posts.js
@@ -8,8 +8,8 @@ module.exports = {
 };
 
 function index(req, res) {
-  Post.find({}, function(err, posts) {
-    res.render('posts/', { title: 'All Posts', posts });
+  Post.find({}, function(err, posts, user) {
+    res.render('posts/show', { title: 'All Posts', posts, user });
   });
 };
 
