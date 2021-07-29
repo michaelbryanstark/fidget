6958125bb7b99c404e787e527a0c7d7b2c5a23fc
Jared Stearne <jared.stearne@gmail.com>
2021-07-28 19:58:21 -0700

    Splash page requires log-in to see other features. Reduces the amoount of unlogged (user) errors. (user) is not getting passed through as variable to posts/new.ejs and comments.ejs. Move on for now.

--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 const postCtrl = require('../controllers/posts'); 
 
 router.get('/', postCtrl.index);
+
 router.get('/new', postCtrl.new);
 router.get('/:id', postCtrl.show); 
 router.post('/show', postCtrl.create); 
 
 module.exports = router;
+
