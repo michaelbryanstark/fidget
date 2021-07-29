e925cb668bf89d4c8f60cb69b00e645a44e794be
michaelbryanstark <84143645+michaelbryanstark@users.noreply.github.com>
2021-07-29 12:02:07 -0400

    start route to delete posts

--- routes/posts.js
+++ routes/posts.js
@@ -5,5 +5,6 @@ router.get('/', postCtrl.index);
 router.get('/new', postCtrl.new);
 router.get('/:id', postCtrl.show); 
 router.post('/', postCtrl.create); 
+router.delete('/:id', postCtrl.deletePost);
 
 module.exports = router;
