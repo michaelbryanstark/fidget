d6ee6ae2b0b490d46b659d335092ab4e16622f64
Jared Stearne <jared.stearne@gmail.com>
2021-07-28 20:33:08 -0700

    Add a little more styling to page. Background color gradient.

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const postCtrl = require('../controllers/posts'); 
 
 router.get('/', postCtrl.index);
-
 router.get('/new', postCtrl.new);
 router.get('/:id', postCtrl.show); 
 router.post('/show', postCtrl.create); 
