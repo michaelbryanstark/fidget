b1639362d543c0ca8cd6d06b3bc6fdb7e0bf2c7a
Jared Stearne <jared.stearne@gmail.com>
2021-07-31 13:09:58 -0700

    Refactored comment update as a post route.

--- routes/comments.js
+++ routes/comments.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const commentsCtrl = require('../controllers/comments');
 
+
 router.get('/posts/:id/comments', isLoggedIn, commentsCtrl.index)
 router.post('/posts/:id/comments',isLoggedIn, commentsCtrl.create);
 router.delete('comments/:id',isLoggedIn, commentsCtrl.deleteComment);
-router.put('comments/:id', isLoggedIn, commentsCtrl.updateComment);
+
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const postCtrl = require('../controllers/posts'); 
+const commentsCtrl = require('../controllers/comments');
 
 router.get('/', postCtrl.index);
 router.get('/new', postCtrl.new);
 router.get('/:id', postCtrl.show); 
 router.post('/', postCtrl.create); 
 router.delete('/:id', postCtrl.deletePost);
-
+router.put('posts/:id', commentsCtrl.updateComment);
 
 module.exports = router;
